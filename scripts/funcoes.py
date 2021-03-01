4fc6254bc2bf117bd0a4796c2db9044e7d5691b6
sandro emilio <sandro.filho@economia.ufjf.br>
2021-03-01 10:46:37 -0300

    Correção de erros e avaliação dos modelos

--- scripts/funcoes.py
+++ scripts/funcoes.py
@@ -1,2 +1,16 @@
 def infoclientesum(base,carac):
    return base.groupby(carac)['CLIENTNUM'].count()
+
+
+def matriz_de_confusão(modelo,X_train, y_train, X_test, ytest):
+   return (modelo.fit(X_train, y_train)
+            ypred = m.predict(X_test)
+
+
+    tn, fp, fn, tp = confusion_matrix(ytest, ypred).ravel()
+    print("True Positive: " + str(tp))
+    print("True Negative: " + str(tn))
+    print("False Positive: " + str(fp))
+    print("False Negative: " + str(fn))
+       
+   )
